02b542d21f7675c0857991c842f6493197b96413
Eduardo <egh1795@gmail.com>
2018-10-18 02:23:34 -0500

    Update the User model to support Omniauth
    Its necessary add a new column called uid to the model that Devise
    generated.
    How I used omniauth-twitter, I've got the access tokens from the OAuth
    service provider from my twitter developer account.
    Well, after all that, I updated the Devise Initializer to add my tokens
    Then I edited the User model, to add the omniauthable module, and
    specify the omniauth provider. In this step also I created the
    'from_omniauth' method.
    Finally I added a controller to handle the Callback URL.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -287,4 +287,23 @@ Devise.setup do |config|
   # ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   # end
+  Devise.setup do |config|
+    #Replace example.com with your own domain name
+    config.mailer_sender = 'egh1795@gmail.com'
+  
+    require 'devise/orm/active_record'
+    config.case_insensitive_keys = [ :email ]
+    config.strip_whitespace_keys = [ :email ]
+    config.skip_session_storage = [:http_auth]
+    config.stretches = Rails.env.test? ? 1 : 10
+    config.reconfirmable = true
+    config.expire_all_remember_me_on_sign_out = true
+    config.password_length = 8..128
+    config.reset_password_within = 6.hours
+    config.sign_out_via = :delete
+  
+    #Add your ID and secret here
+    #ID first, secret second
+    config.omniauth :twitter, "pWsyhnDVeRBUYorSu52TlTxlB", "IP4PlRSuiXWHL7BMsLMB3f3A8qZuFbytnSNNyWfaWb3UWEvej8"
+  end
 end
