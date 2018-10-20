a570998ea05f3668208097bb4514597c90abd55d
Eduardo <egh1795@gmail.com>
2018-10-20 09:07:30 -0500

    Improve views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -32,7 +32,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.html { redirect_to root_path, notice: 'List was successfully created.' }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
@@ -58,6 +58,7 @@ class ListsController < ApplicationController
   # DELETE /lists/1
   # DELETE /lists/1.json
   def destroy
+    @list.tasks.each{|task| task.destroy}
     @list.destroy
     respond_to do |format|
       format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+    def delete_to_with_icon(text, path, classes)
+        form_tag path, :method => :delete do
+            button_tag(classes) do
+            raw text
+            end
+        end
+    end
+
+    def post_to_with_icon(text, path, classes)
+        form_tag path, :method => :delete do
+            button_tag(classes) do
+            raw text
+            end
+        end
+    end
+
+    def get_to_with_icon(text, path, classes)
+        form_tag path, :method => :get do
+            button_tag(classes) do
+            raw text
+            end
+        end
+    end
 end
