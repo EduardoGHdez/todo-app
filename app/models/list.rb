fd50929311949cba9b7ec95b34c6201040dc9ba9
Eduardo <egh1795@gmail.com>
2018-10-25 17:18:21 -0500

    Add export CSV function
    I re-used the 'exportable' controller, to switch between pdf or csv
    export. I added a function into List model. This function generate a
    CSV file with all current user  tasks.
    
    The user can run this function with a button added into the navbar.
    To see it, user must hover his name. There is a dropdown with
    a group of buttons

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,15 @@
 class List < ApplicationRecord
   has_many :tasks
   belongs_to :user
+
+  # Generate a CSV File of All Movie Records
+  def self.to_csv(fields = column_names, options={})
+    CSV.generate(options) do |csv|
+      all.each do |list|
+        list.tasks.all.each do |task|
+          csv << [list.title ,task.attributes.values_at(*fields)]
+        end
+      end
+    end
+  end
 end
