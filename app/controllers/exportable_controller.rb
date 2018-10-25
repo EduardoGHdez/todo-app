06e8aab902384ed8fac00d1643d1675d4ff1dce8
Eduardo <egh1795@gmail.com>
2018-10-25 16:16:37 -0500

    Add function to export pdf files
    Now users can download a pdf with their current lists and tasks.
    This function is located into the navbar. Is visible when the user hover
    his name.
    To made this, I created an 'exportable' controller, here I wrote the
    'export' method. In this controller I'll make a function to export CSV
    files with the same data.
    By the way. The main toot that I used is Prawn. It's a very useful gem
    to generate PDF's

---
+++ app/controllers/exportable_controller.rb
@@ -0,0 +1,31 @@
+class ExportableController < ApplicationController
+    def export
+        @lists = current_user.lists.all
+        respond_to do |format|
+          format.pdf do
+            pdf = Prawn::Document.new
+            #PDF content
+            if !(@lists.empty?)
+              @lists.each do |list|
+                pdf.text list.title
+                if !(list.tasks.empty?)
+                  data = list.tasks.collect{ |task| [ task.title, task.due_date ] }
+                  pdf.table(data, :cell_style => {
+                    :position => :center,
+                    :width => 200,
+                    :borders => [:bottom, :top]
+                  })
+                else
+                  pdf.text "List without tasks!", :size => 15, :align => :center
+                end
+                pdf.move_down 20
+              end
+              send_data pdf.render,
+                filename: "export.pdf",
+                type: 'application/pdf',
+                disposition: 'inline'
+            end
+          end
+        end
+      end
+end
