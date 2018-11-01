98d754d4463c49e2db8216e06c143468068842af
Eduardo <egh1795@gmail.com>
2018-11-01 01:25:59 -0600

    Setup Production env.
    I add Gmail and sidekiq confit into production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,33 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "todo_app_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'rails.examples@gmail.com'}
+
+  #Specific config for Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            'rails.examples',
+      password:             'todoapp123',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+# ActiveJob setup
+  config.active_job.queue_adapter = :sidekiq
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -89,4 +115,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
 end
