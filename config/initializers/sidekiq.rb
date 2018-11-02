df1681281cd0b362151df050eaae7e9bbffb3d81
Eduardo <egh1795@gmail.com>
2018-11-02 07:13:22 -0600

    Schedule Reminder every Monday at 9 o'clock
    I made this to make a weekly remider

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
-if Rails.env.production?
+
+require 'sidekiq/scheduler'
+
+#if Rails.env.production?
+  Sidekiq.schedule = YAML.load_file(File.expand_path('../../../config/schedule.yml',__FILE__))
+  Sidekiq::Scheduler.load_schedule! # This will retrigger the loading stage
 
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV['REDIS_URL'], size: 2 }
@@ -9,8 +14,8 @@ if Rails.env.production?
     config.average_scheduled_poll_interval = 15
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
+      ActiveRecord::Base.connection_pool.disconnect!      
     end
   end
-
-end
+  
+#end
